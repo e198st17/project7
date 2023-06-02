91bf7ef3b12f89269dbc24870dcd465087bc7b76
Lucas Ferns <lbferns@ucdavis.edu>
2023-06-01 21:13:22 -0700

    Error checking for first argument

--- webdiff.sh
+++ webdiff.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-printf "I love Aimee HEHEHEHEHEHEHE\n"
+#Lucas Ferns, Project7
+#EVE198
+#webdiff.sh script
+
+#Checks if the first argument is 'add'
+if [[ "$1" = "add" ]]
+then
+    printf "Your first argument was add!"
+
+
+#Chekcs if the first argument is 'check'
+elif [[ "$1" = "check" ]]
+then
+   printf "Your first argument was check!"
+
+
+#Checks if the first arugment is 'list'
+elif [[ "$1" = "list" ]]
+then
+   printf "Your first argument was list!"
+
+
+#Error message for incorrect first argument
+else
+    printf "Error, first arugment must be 'add', 'check', or 'list'\n"
+fi
+       
+    
