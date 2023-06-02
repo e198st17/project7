05328b2428ed9519dd65e70e93c6224add16ea55
Lucas Ferns <lbferns@ucdavis.edu>
2023-06-01 22:18:57 -0700

    Creates the .webd file in home directory

--- webdiff.sh
+++ webdiff.sh
@@ -2,12 +2,20 @@
 
 #Lucas Ferns, Project7
 #EVE198
-#webdiff.sh script
+#webdiff.sh
+
+#creates a variable for the file that is created
+WEB_FILE=~/.webd
+
+if ! [[ -f "$HOME/.webd" ]] #Checks if the file does not exist in the home directory
+then
+    touch $HOME/.webd #Creates the file if it does not already exist
+fi
 
 #Checks if the first argument is 'add'
 if [[ "$1" = "add" ]]
 then
-    if [[ $# -ne 2 ]]
+    if [[ $# -ne 2 ]] #Checks that there are 2 arguments
     then
 	printf "Error: Must be two arguments when 'add' is the first argument\n"
     fi
@@ -16,7 +24,7 @@ then
 #Chekcs if the first argument is 'check'
 elif [[ "$1" = "check" ]]
 then
-    if [[ $# -ne 2 ]]
+    if [[ $# -ne 2 ]] #Checks that there are 2 arguments
     then
 	printf "Error: Must be two arguments when 'check' is the first argument\n"
     fi
@@ -25,10 +33,12 @@ then
 #Checks if the first arugment is 'list'
 elif [[ "$1" = "list" ]]
 then
-    if [[ $# -ne 1 ]]
+    if [[ $# -ne 1 ]] #Checks that there is only one arugment
     then
 	printf "Error: Must be only one argument when 'list' argument is used\n"
-    fi 
+    fi
+    awk '{ print $1 }' "$WEB_FILE" #prints the first column of webd file (the nicknames)
+   
 
 
 #Error message for incorrect first argument
